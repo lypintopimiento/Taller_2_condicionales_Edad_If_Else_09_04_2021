12ff37b81e5a4d2a3fa772b8756acb58c6f0cce6
lypintopimiento <lypintopimiento@gmail.com>
2021-04-21 19:00:15 -0500

    Taller_2_condicionales_Edad_If_Else_09_04_2021

---
+++ main.js
@@ -0,0 +1,12 @@
+
+ let edad= parseInt(prompt ("Ingrese su edad:"));
+ 
+ console.warn("La edad ingresada es:");
+ console.log(edad);
+ console.warn("Respuesta:");
+if (edad>=18)
+    {
+    console.log(`La persona es mayor de edad`);
+    } else {
+    console.log(`La edad ingresada no cumple con los parámetros`);
+}
